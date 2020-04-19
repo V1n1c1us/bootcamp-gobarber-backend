6345362416fe64d58a498a6fd2846630fd30631f
V1n1c1us <v.f.diehl@gmail.com>
2020-04-19 17:04:47 -0300

    Authenticating and generating JWT token for user

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import appoitmentsRouter from './appointments.routes';
 import usersRouter from './users.routes';
+import sessionRouter from './sessions.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appoitmentsRouter);
 routes.use('/users', usersRouter);
+routes.use('/sessions', sessionRouter);
 
 export default routes;
