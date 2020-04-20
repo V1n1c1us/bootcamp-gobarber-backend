6c5f7979aafe2c209a6a9fce2904afcb22df599f
V1n1c1us <v.f.diehl@gmail.com>
2020-04-19 21:36:38 -0300

    Exception handling

--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -3,6 +3,8 @@ import { getCustomRepository } from 'typeorm';
 import Appointment from '../models/Appointment';
 import AppointmentRespository from '../repositories/AppointmentsRepository';
 
+import AppError from '../errors/AppError';
+
 interface Request {
   provider_id: string;
   date: Date;
@@ -22,7 +24,7 @@ class CreateAppointmentService {
     );
 
     if (findAppointmentInSameDate) {
-      throw Error('This Appointments is already booked');
+      throw new AppError('This Appointments is already booked');
     }
 
     const appointment = appointmentsRepository.create({
