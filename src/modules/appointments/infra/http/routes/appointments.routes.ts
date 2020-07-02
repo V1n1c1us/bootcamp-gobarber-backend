a991e4a67df864994ae8f181bc11ed1c968c4d37
V1n1c1us <v.f.diehl@gmail.com>
2020-07-02 00:11:32 -0300

    List Providers

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -2,12 +2,16 @@ import { Router } from 'express';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import AppointmentsController from '../controllers/AppointmentsControllers';
+import ProviderAppointmentsController from '../controllers/ProviderAppointmentsController';
 
 const appoitmentsRouter = Router();
+
 const appointmentsController = new AppointmentsController();
+const providerAppointmentsController = new ProviderAppointmentsController();
 
 appoitmentsRouter.use(ensureAuthenticated);
 
 appoitmentsRouter.post('/', appointmentsController.create);
+appoitmentsRouter.get('/me', providerAppointmentsController.index);
 
 export default appoitmentsRouter;
