13c377aeef84019eb9e847680478ed610ec8e575
V1n1c1us <v.f.diehl@gmail.com>
2020-07-02 23:19:53 -0300

    Apply class-transformer

--- src/modules/users/infra/http/controllers/UsersController.ts
+++ src/modules/users/infra/http/controllers/UsersController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import CreateUserService from '@modules/users/services/CreateUserService';
 
@@ -16,9 +17,8 @@ export default class UsersController {
         password,
       });
 
-      delete user.password;
+      return response.json(classToClass(user));
 
-      return response.json(user);
     } catch (err) {
       return response.status(400).json({ error: err.message });
     }
